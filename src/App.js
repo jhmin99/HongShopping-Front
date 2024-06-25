4c7b8e08c30dc96f2dfdf4c6a1039dbbe6a95525
jhmin99 <mjh012399@gmail.com>
2024-06-25 18:30:30 +0900

    style: Add semicolon for consistency in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from './feat/Login';
 import Home from './feat/Home';
 import Logout from './feat/Logout';
 import UserDetails from './feat/UserDetails';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 
 function App() {
@@ -12,13 +12,13 @@ function App() {
 
   return (
     <CookiesProvider>
-        <Routes>
-          <Route path='/home' element={<Home />} />
-          <Route path='/signin' element={<Login />} />
-          <Route path='/signup' element={<SignUp />} />
-          <Route path='/users/:id' element={<UserDetails />} />
-          <Route path='/logout' element={<Logout />} />
-        </Routes>
+      <Routes>
+        <Route path='/home' element={<Home />} />
+        <Route path='/signin' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/users/:id' element={<UserDetails />} />
+        <Route path='/logout' element={<Logout />} />
+      </Routes>
     </CookiesProvider>
 
 
