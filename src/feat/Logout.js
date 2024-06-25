e43a11f1e735c1c250f6c99f90af9eeaf3dfbb3e
jhmin99 <mjh012399@gmail.com>
2024-06-25 18:28:31 +0900

    feat: Add logout functionality with session management and redirection

--- src/feat/Logout.js
+++ src/feat/Logout.js
@@ -1,40 +1,36 @@
-import { Navigate } from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
-import { useState } from 'react';
-import axios from '../config/Interceptor.js';
+import { useState, useEffect } from 'react';
 import axiosInstance from '../config/Interceptor.js';
 
 function Logout() {
     const [isRedirected, setIsRedirect] = useState(false);
-    
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axiosInstance.post(
-                '/api/logout'
-            );
+            await axiosInstance.post('/api/logout');
             console.log('Logout success');
-            if(sessionStorage.getItem('userdetails')){
-                sessionStorage.removeItem('userdetails');
-            }
+            sessionStorage.removeItem('userdetails');  // 사용자 정보 제거
             setIsRedirect(true);
         } catch (error) {
             console.error('Logout failed:', error);
         }
     };
 
+    useEffect(() => {
+        if (isRedirected) {
+            window.location.reload();  // 페이지 새로고침
+        }
+    }, [isRedirected]);
+
     return (
         <Container>
-            <p className='title'>
-                Logout
-            </p>
+            <p className='title'>Logout</p>
+            <button type="button" onClick={handleSubmit}>Logout</button>
             {isRedirected && <Navigate to="/home" />}
-            <button type="button" onClick={handleSubmit}>
-                Logout
-            </button>
-            {isRedirected && <Navigate to = "/home"/>}
         </Container>
     );
 }
+
 export default Logout;
