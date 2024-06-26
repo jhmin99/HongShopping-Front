31a523e4028c94b432cc3f2cfb326cc5e6cdc379
jhmin99 <mjh012399@gmail.com>
2024-06-26 19:00:02 +0900

    refactor: Enhance App component with session timeout and automatic redirection
    - Added session timeout logic to automatically log out users after 30 minutes of inactivity.
    - Updated event listeners to reset session timeout on user activity.
    - Added logic to clear session storage and redirect to login page on session timeout.
    - Ensured server-side logout is called if session becomes inactive.
    - Changed `/users/:id` route to `/mypage` for user details.
    - Added default route to redirect to `/home`.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+import Home from './feat/Home';
 import SignUp from './feat/SignUp';
 import Login from './feat/Login';
-import Home from './feat/Home';
 import Logout from './feat/Logout';
 import UserDetails from './feat/UserDetails';
-import { Routes, Route } from 'react-router-dom';
-import { CookiesProvider } from 'react-cookie';
+import axiosInstance from '../src/config/Interceptor.js';
 
 function App() {
+  const [isSessionActive, setIsSessionActive] = useState(true);
 
+  const resetTimeout = useCallback(() => {
+    setIsSessionActive(true);
+    if (window.sessionTimeout) {
+      clearTimeout(window.sessionTimeout);
+    }
+    window.sessionTimeout = setTimeout(() => {
+      setIsSessionActive(false);
+      sessionStorage.removeItem('userdetails');
+      window.location.href = '/signin';
+    }, 1800000); // 30분
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', resetTimeout);
+    window.addEventListener('keydown', resetTimeout);
+    resetTimeout();
+
+    return () => {
+      window.removeEventListener('mousemove', resetTimeout);
+      window.removeEventListener('keydown', resetTimeout);
+    };
+  }, [resetTimeout]);
+
+  useEffect(() => {
+    if (!isSessionActive) {
+      axiosInstance.post('/api/logout'); // 필요한 경우 서버 로그아웃도 호출
+    }
+  }, [isSessionActive]);
 
   return (
     <CookiesProvider>
@@ -16,13 +46,11 @@ function App() {
         <Route path='/home' element={<Home />} />
         <Route path='/signin' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
-        <Route path='/users/:id' element={<UserDetails />} />
+        <Route path='/mypage' element={<UserDetails />} />
         <Route path='/logout' element={<Logout />} />
+        <Route path='/' element={<Navigate to='/home' />} />
       </Routes>
     </CookiesProvider>
-
-
-
   );
 }
 
