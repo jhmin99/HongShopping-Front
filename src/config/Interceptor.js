dca7f7bc0eb31153a6fd1358607a96c318aad560
jhmin99 <mjh012399@gmail.com>
2024-06-28 15:29:52 +0900

    refactor: update axiosInstance for CSRF token handling and improved authentication

--- src/config/Interceptor.js
+++ src/config/Interceptor.js
@@ -1,10 +1,23 @@
 import axios from 'axios';
+import { setCookie } from '../utils/CookieUtils';
 
 // axios의 인스턴스 생성
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080/', // API의 기본 URL
+    baseURL: 'http://localhost:8080/api/', // API의 기본 URL
     withCredentials: true, // 요청에 자격 증명 정보를 포함
 });
+
+// CSRF 토큰 요청 함수
+const fetchCsrfToken = async () => {
+    try {
+        const response = await axiosInstance.get('/csrf-token');
+        const csrfToken = response.data.token;
+        setCookie('XSRF-TOKEN', csrfToken);
+        return csrfToken;
+    } catch (error) {
+        console.error('Error fetching CSRF token', error);
+    }
+};
 
 // 요청 인터셉터 설정
 axiosInstance.interceptors.request.use(
@@ -13,6 +26,13 @@ axiosInstance.interceptors.request.use(
 
         if (userdetails && userdetails.accessToken) {
             request.headers['Authorization'] = `Bearer ${userdetails.accessToken}`;
+        }
+
+        // CSRF 토큰을 쿠키에서 읽어서 헤더에 추가
+        const csrfCookie = document.cookie.split('; ').find(row => row.startsWith('XSRF-TOKEN'));
+        if (csrfCookie) {
+            const csrfToken = csrfCookie.split('=')[1];
+            request.headers['X-XSRF-TOKEN'] = csrfToken;
         }
 
         return request;
@@ -37,7 +57,7 @@ axiosInstance.interceptors.response.use(
 
             try {
                 // Refresh token을 사용하여 새로운 access token 요청
-                const response = await axiosInstance.post('/api/refresh-token', {
+                const response = await axiosInstance.post('/refresh-token', {
                     refreshToken: userdetails.refreshToken
                 });
 
@@ -47,11 +67,7 @@ axiosInstance.interceptors.response.use(
 
                 // 원래의 요청을 새로운 access token으로 다시 시도
                 originalRequest.headers['Authorization'] = `Bearer ${userdetails.accessToken}`;
-                await axiosInstance(originalRequest);
-
-                // 새로운 access token이 유효하므로 /home으로 리다이렉션
-                window.location = '/home';
-                return;
+                return axiosInstance(originalRequest);
             } catch (refreshError) {
                 // Refresh token 만료 시 세션 스토리지를 비우고 로그인 페이지로 리다이렉션
                 if (refreshError.response && refreshError.response.status === 403) {
@@ -61,10 +77,14 @@ axiosInstance.interceptors.response.use(
                 return Promise.reject(refreshError);
             }
         }
-
+        
+        if (error.response) {
+            // CSRF 토큰이 잘못된 경우, 토큰을 다시 갱신
+            await fetchCsrfToken();
+        }
         // 기타 에러 처리
         return Promise.reject(error);
     }
 );
 
-export default axiosInstance;
+export { axiosInstance, fetchCsrfToken };
