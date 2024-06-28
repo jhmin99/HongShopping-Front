3474d551e66a38aa640210254a4c91d289d1c726
jhmin99 <mjh012399@gmail.com>
2024-06-28 15:33:42 +0900

    refactor: enhance UserDetails component with delivery address management and UI improvements

--- src/feat/UserDetails.js
+++ src/feat/UserDetails.js
@@ -1,14 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { Navigate } from 'react-router-dom';
-import axiosInstance from '../config/Interceptor.js';
+import { axiosInstance, fetchCsrfToken } from '../config/Interceptor.js';
+import { Container, Row, Col, Card, Form, Button, ListGroup, Alert } from 'react-bootstrap';
+import '../css/UserDetails.css';
 
 function UserDetails() {
     const [userDetails, setUserDetails] = useState(null);
     const [userId, setUserId] = useState(null);
     const [isRedirected, setIsRedirect] = useState(false);
+    const [newAddress, setNewAddress] = useState({
+        name: '',
+        phoneNumber: '',
+        zipCode: '',
+        address: '',
+        addressDetail: ''
+    });
+    const [editingAddressId, setEditingAddressId] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
     useEffect(() => {
-        // sessionStorage에서 userdetails 가져오기
         const storedUserDetails = sessionStorage.getItem('userdetails');
         if (storedUserDetails) {
             const parsedDetails = JSON.parse(storedUserDetails);
@@ -16,56 +26,213 @@ function UserDetails() {
         } else {
             setIsRedirect(true);
         }
+
+        // 페이지 로드 시 CSRF 토큰 갱신
+        fetchCsrfToken();
     }, []);
+
+    const fetchUserDetails = useCallback(async () => {
+        try {
+            const response = await axiosInstance.get(`/users?userId=${userId}`);
+            setUserDetails(response.data);
+        } catch (error) {
+            if (error.response && error.response.status === 401) {
+                setIsRedirect(true);
+            } else {
+                console.log(error);
+            }
+        }
+    }, [userId]);
 
     useEffect(() => {
         if (!userId) return;
 
-        // 사용자 정보를 가져오는 API 호출
-        const fetchUserDetails = async () => {
-            try {
-                const response = await axiosInstance.get(`/api/users?userId=${userId}`);
-                setUserDetails(response.data);
-            } catch (error) {
-                if (error.response && error.response.status === 401) {
-                    setIsRedirect(true);
-                } else {
-                    console.log(error);
-                }
-            }
-        };
-
         fetchUserDetails();
-    }, [userId]);
+    }, [userId, fetchUserDetails]);
 
-    const addDeliveryAddress = () => {
-        // 배송지 추가하는 로직
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setNewAddress({ ...newAddress, [name]: value });
+    };
+
+    const addOrUpdateDeliveryAddress = async () => {
+        try {
+            setErrorMessage(''); // Clear error message
+            if (editingAddressId) {
+                await axiosInstance.put(`/users/delivery-address/${editingAddressId}`, {
+                    userId: userId,
+                    ...newAddress
+                });
+                alert('Delivery address updated successfully');
+            } else {
+                await axiosInstance.post('/users/delivery-address', {
+                    userId: userId,
+                    ...newAddress
+                });
+                alert('Delivery address added successfully');
+            }
+            fetchUserDetails();
+            setNewAddress({
+                name: '',
+                phoneNumber: '',
+                zipCode: '',
+                address: '',
+                addressDetail: ''
+            });
+            setEditingAddressId(null);
+        } catch (error) {
+            console.error('Error adding/updating delivery address', error);
+            setErrorMessage('Error adding/updating delivery address');
+        }
+    };
+
+    const editDeliveryAddress = (address) => {
+        setErrorMessage(''); // Clear error message
+        setNewAddress({
+            name: address.name,
+            phoneNumber: address.phoneNumber,
+            zipCode: address.zipCode,
+            address: address.address,
+            addressDetail: address.addressDetail
+        });
+        setEditingAddressId(address.id);
+    };
+
+    const deleteDeliveryAddress = async (addressId) => {
+        try {
+            setErrorMessage(''); // Clear error message
+            await axiosInstance.delete(`/users/delivery-address/${addressId}`);
+            alert('Delivery address deleted successfully');
+            fetchUserDetails();
+            setNewAddress({
+                name: '',
+                phoneNumber: '',
+                zipCode: '',
+                address: '',
+                addressDetail: ''
+            });
+            setEditingAddressId(null);  // Reset form to "Add" mode
+        } catch (error) {
+            console.error('Error deleting delivery address', error);
+            setErrorMessage('Error deleting delivery address');
+        }
     };
 
     return (
-        <div>
-            <h1>My Page</h1>
+        <Container>
+            <h1 className="my-4">My Page</h1>
             {userDetails ? (
-                <div>
-                    <p>User ID: {userDetails.identification}</p>
-                    <p>Username: {userDetails.name}</p>
-                    <p>BirthDate: {userDetails.birthDate}</p>
-                    <p>PhoneNumber: {userDetails.phoneNumber}</p>
-                    <p>Delivery Addresses: </p>
-                    <ul>
-                        {userDetails.deliveryAddresses.map(address => (
-                            <li key={address.deliveryAddressId}>
-                                {address.name}, {address.phoneNumber}, {address.address}, {address.addressDetail}, {address.zipCode}
-                            </li>
-                        ))}
-                    </ul>
-                    <button onClick={addDeliveryAddress}>Add Delivery Address</button>
-                </div>
+                <Row>
+                    <Col md={6}>
+                        <Card>
+                            <Card.Header>User Information</Card.Header>
+                            <Card.Body>
+                                <p><strong>User ID:</strong> {userDetails.identification}</p>
+                                <p><strong>Username:</strong> {userDetails.name}</p>
+                                <p><strong>BirthDate:</strong> {userDetails.birthDate}</p>
+                                <p><strong>PhoneNumber:</strong> {userDetails.phoneNumber}</p>
+                            </Card.Body>
+                        </Card>
+                        <Card className="mt-4">
+                            <Card.Header>Delivery Addresses</Card.Header>
+                            <Card.Body>
+                                <ListGroup>
+                                    {userDetails.deliveryAddresses.map(address => (
+                                        <ListGroup.Item key={address.id} className="list-group-item">
+                                            <span>{address.name}, {address.phoneNumber}, {address.address}, {address.addressDetail}, {address.zipCode}</span>
+                                            <div>
+                                                <Button variant="custom-edit btn-custom me-2" onClick={() => editDeliveryAddress(address)}>Edit</Button>
+                                                <Button variant="custom-delete btn-custom" onClick={() => deleteDeliveryAddress(address.id)}>Delete</Button>
+                                            </div>
+                                        </ListGroup.Item>
+                                    ))}
+                                </ListGroup>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col md={6}>
+                        <Card>
+                            <Card.Header>{editingAddressId ? 'Edit Delivery Address' : 'Add New Delivery Address'}</Card.Header>
+                            <Card.Body>
+                                <Form>
+                                    <Row>
+                                        <Col md={6}>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Name</Form.Label>
+                                                <Form.Control
+                                                    type="text"
+                                                    name="name"
+                                                    value={newAddress.name}
+                                                    onChange={handleInputChange}
+                                                    placeholder="Name"
+                                                />
+                                            </Form.Group>
+                                        </Col>
+                                        <Col md={6}>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Phone Number</Form.Label>
+                                                <Form.Control
+                                                    type="text"
+                                                    name="phoneNumber"
+                                                    value={newAddress.phoneNumber}
+                                                    onChange={handleInputChange}
+                                                    placeholder="Phone Number"
+                                                />
+                                            </Form.Group>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col md={4}>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Zip Code</Form.Label>
+                                                <Form.Control
+                                                    type="text"
+                                                    name="zipCode"
+                                                    value={newAddress.zipCode}
+                                                    onChange={handleInputChange}
+                                                    placeholder="Zip Code"
+                                                />
+                                            </Form.Group>
+                                        </Col>
+                                        <Col md={4}>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Address</Form.Label>
+                                                <Form.Control
+                                                    type="text"
+                                                    name="address"
+                                                    value={newAddress.address}
+                                                    onChange={handleInputChange}
+                                                    placeholder="Address"
+                                                />
+                                            </Form.Group>
+                                        </Col>
+                                        <Col md={4}>
+                                            <Form.Group className="mb-3">
+                                                <Form.Label>Address Detail</Form.Label>
+                                                <Form.Control
+                                                    type="text"
+                                                    name="addressDetail"
+                                                    value={newAddress.addressDetail}
+                                                    onChange={handleInputChange}
+                                                    placeholder="Address Detail"
+                                                />
+                                            </Form.Group>
+                                        </Col>
+                                    </Row>
+                                    <Button variant="primary mt-3" onClick={addOrUpdateDeliveryAddress}>
+                                        {editingAddressId ? 'Update' : 'Add'} Delivery Address
+                                    </Button>
+                                </Form>
+                                {errorMessage && <Alert variant="danger" className="mt-3">{errorMessage}</Alert>}
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             ) : (
                 <p>Loading user details...</p>
             )}
-            {isRedirected && <Navigate to="/signin" />}
-        </div>
+            {isRedirected && <Navigate to="/signin" replace />}
+        </Container>
     );
 }
 
